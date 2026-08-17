7a73ba23ad6cec0793880b42fc85c05884a4bcb5
Thifany Murakami <thifanymurakami28@gmail.com>
2026-08-17 15:03:24 -0300

    mensagem toast

--- app-marcador-truco/src/app/home/home.page.ts
+++ app-marcador-truco/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonButton, IonIcon, ToastController } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { add, remove } from 'ionicons/icons';
 @Component({
@@ -15,20 +15,43 @@ export class HomePage {
 
   public placarN: number = 0;
   public placarE: number = 0;
+  private toastController: ToastController = inject(ToastController);
+
+
+  private async exibirMensagem(mensagem: string) {
+    const toast = await this.toastController.create({
+      message: mensagem,
+      duration: 2500,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
 
   protected adicionarNos() {
     this.placarN = this.placarN + 1;
+    if (this.placarN == 12) {
+      this.exibirMensagem("Nós - VENCEDOR");
+    }
   }
 
   protected adicionarEles() {
     this.placarE = this.placarE + 1;
+    if (this.placarE == 12) {
+      this.exibirMensagem("Eles - VENCEDOR");
+    }
   }
 
   protected removerNos() {
+    if (this.placarN > 0) {
       this.placarN = this.placarN - 1;
+    }
   }
 
   protected removerEles() {
-    this.placarE = this.placarE - 1;
+    if (this.placarE > 0) {
+      this.placarE = this.placarE - 1;
+    }
+
   }
 }
